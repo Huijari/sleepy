fcbec273e9547470ea63b32584c3ed6c2cc90558
Alexandre Cesar <acmorcego@gmail.com>
2017-05-28 14:05:07 -0300

    Style input

--- a/input.js
+++ a/input.js
@@ -2,12 +2,15 @@ import h from 'inferno-hyperscript'
 import moist from 'moisture'
 import { of } from 'most'
 
+const inputClassName = 'input-reset bn mh2 bg-purple white'
+
 const Input = (label, type) => {
   const value$ = moist()
 
   const vtree$ = of(h('label', [
     `${label}`,
     h('input', {
+      className: inputClassName,
       type,
       oninput: e => value$.emit(e.target.value)
     })
