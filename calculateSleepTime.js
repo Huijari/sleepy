08ec26ea0f90626ea626acfbcbec0284d094612a
Alexandre Cesar <acmorcego@gmail.com>
2017-05-28 11:23:25 -0300

    Compute sleep times

---
+++ calculateSleepTime.js
@@ -0,0 +1,29 @@
+const min = 3
+const interval = 1.5
+const quantity = 4
+
+const calculateSleepTime = time => {
+  const date = new Date()
+  date.setHours(time.split(':')[0])
+  date.setMinutes(time.split(':')[1])
+
+  const points = (new Array(quantity))
+    .fill(0)
+    .map((v, k) => min + (k + 1) * interval)
+
+  const sleepTimes = points
+    .map(point => {
+      const sleepDate = new Date()
+      sleepDate.setHours(date.getHours() - Math.floor(point))
+      sleepDate.setMinutes(date.getMinutes() - 60 * (point % 1))
+      return sleepDate
+    })
+    .map(sleepDate => sleepDate
+      .toLocaleTimeString()
+      .replace(/:\d\d\s/, ' ')
+    )
+    .reverse()
+  return sleepTimes
+}
+
+export default calculateSleepTime
