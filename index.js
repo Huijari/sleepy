f7c1704ae66a35f178475d830045a706924b4660
Alexandre Cesar <acmorcego@gmail.com>
2017-05-28 11:33:46 -0300

    Assembly basic layout

---
+++ index.js
@@ -0,0 +1,31 @@
+import inferno from 'inferno'
+import { combine, merge, of } from 'most'
+
+import Div from './div'
+import SleepAt from './sleepAt'
+import WakeUpAt from './wakeUpAt'
+
+import calculateSleepTime from './calculateSleepTime'
+
+const wakeUpAt = WakeUpAt()
+const sleepAt = SleepAt(
+  wakeUpAt.value$
+    .map(calculateSleepTime)
+    .map(times => times.join(' or '))
+)
+
+const bottom = merge(
+  of(Div()),
+  sleepAt.vtree$.sampleWith(wakeUpAt.value$)
+)
+
+const vtree$ = combine(
+  Div,
+  wakeUpAt.vtree$,
+  bottom
+)
+
+const el = document.querySelector('main')
+vtree$.observe(vtree =>
+  inferno.render(vtree, el)
+)
